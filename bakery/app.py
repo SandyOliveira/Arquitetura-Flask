1ed0ed620170cbd3616ab454a2227b9448236e2c
SandyOliveira <sandyoliveira828@gmail.com>
2021-03-21 03:20:36 -0300

    projeto organizado em blueprints e extenções

--- bakery/app.py
+++ bakery/app.py
@@ -1,7 +1,10 @@
 from flask import Flask, render_template
 from bakery.ext import configuration 
 from bakery.ext import appearance
-from bakery.ext import views
+
+
+from bakery.blueprints import views
+from bakery.blueprints import restapi
 
 
 app = Flask(__name__)
@@ -9,4 +12,5 @@ app = Flask(__name__)
 configuration.init_app(app)
 appearance.init_app(app)
 views.init_app(app)
+restapi.init_app(app)
 
